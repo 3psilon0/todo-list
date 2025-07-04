b2f17e67e69c7d66fb7ab4c81554095ff42c558f
3psilon0 <bestofluck110@gmail.com>
2025-07-04 11:51:46 +0500

    Minor changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,11 @@ import views from './views/ListViews.js';
 import ListController from './controllers/ListController.js'
 import ListModel from './models/ListModel.js';
 
-const model = new ListModel()
+const model = new ListModel();
 const controller = new ListController(views, model);
 
+controller.initialize();
 document.querySelector('.list-add-button').addEventListener('click', controller.handleListCreate);
+
+
+console.log(model.lists);
