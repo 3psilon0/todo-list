2c1c97fbb3ee59fed18a5d2b734e3a0051a13310
3psilon0 <bestofluck110@gmail.com>
2025-08-29 05:45:04 +0500

    Lists are now displayed sorted in lexographical order after adding or editing the name of a list

--- src/scripts/controllers/ListController.js
+++ src/scripts/controllers/ListController.js
@@ -1,77 +1,100 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-export default class{
-    constructor(ListViews, ListModel){
+export default class {
+    constructor(ListViews, ListModel) {
         this.views = ListViews;
         this.model = ListModel;
     }
 
-    initialize(){
-        this.model.lists.forEach(list => {
-            this.createList(list.id, list.name);
-        });
-        document.querySelector('.list-add-button').addEventListener('click', this.handleListCreate);
-    }
-    
-    createList(listId, listName){
-        this.views.renderListButton(listId, listName);
-        
-        if(this.model.searchList(listId) === undefined){
-            this.model.createList(listId, listName);
-        }
-
-        // Handles list name edit
-        document.querySelector(`div[data-id="${listId}"]`).children.item(2).children.item(0).addEventListener('click', (e) => {
-            this.views.renderListEditButton(listId);
-            const listEditButton = document.querySelector(`div[data-id="${listId}"]`);
-
-            const listEditInput = listEditButton.children.item(0);
-            listEditInput.value = listName;
-            listEditInput.focus();
-
-            // List edit confirm
-            listEditButton.children.item(1).addEventListener("click", () => {
-                let editItem = this.model.searchList(listId);
-                editItem.editListName(listEditInput.value);
-                editItem = this.model.searchList(listId);
-                listEditButton.remove();
-                this.createList(listId, editItem.name);
-            })
-
-            // List edit cancel
-            listEditButton.children.item(2).addEventListener("click", () => {
-                let editItem = this.model.searchList(listId);
-                listEditButton.remove();
-                this.createList(listId, editItem.name);
-            })
-        })
-
-        // Handles removal of list
-        document.querySelector(`div[data-id="${listId}"]`).children.item(2).children.item(1).addEventListener('click', (e) => {
-            document.querySelector(`div[data-id="${listId}"]`).remove();
-            this.model.deleteList(listId);  
-        })
-    }
-
-
     handleListCreate = () => {
-        const navBar = document.querySelector('.navbar');
-        let listCreateButton = document.querySelector('.list-create-button');
+        const navBar = document.querySelector(".navbar");
+        let listCreateButton = document.querySelector(".list-create-button");
 
-        if(!navBar.contains(listCreateButton)){
+        if (!navBar.contains(listCreateButton)) {
+            const handleCreateConfirm = () => {
+                listCreateButton = document.querySelector(".list-create-button");
+                const input = document.querySelector(".list-create-input");
+                this.createList(uuidv4(), input.checkValidity() ? input.value : "Default List");
+                listCreateButton.remove();
+            }
+
+            const handleCreateCancel = () => {
+                listCreateButton = document.querySelector(".list-create-button");
+                listCreateButton.remove();
+            }
+
             this.views.renderListCreateButton();
 
-            document.querySelector('.list-create-confirm').addEventListener('click', () => {
-                listCreateButton = document.querySelector('.list-create-button');
-                const input = document.querySelector('.list-create-input');
-                this.createList(uuidv4(), input.checkValidity() ? input.value : 'Default List')
-                listCreateButton.remove();
-            })
+            document.querySelector(".list-create-confirm").addEventListener("click", handleCreateConfirm);
 
-            document.querySelector('.list-create-cancel').addEventListener('click', () => {
-                listCreateButton = document.querySelector('.list-create-button');
-                listCreateButton.remove();
-            })
+            document.querySelector(".list-create-cancel").addEventListener("click", handleCreateCancel);
         }
+    };
+
+    handleListEdit(listId, listName) {
+        this.views.renderListEditButton(listId);
+        const listEditButton = document.querySelector(`div[data-id="${listId}"]`);
+
+        const listEditInput = listEditButton.children.item(0);
+        listEditInput.value = listName;
+        listEditInput.focus();
+
+        // List edit confirm
+        listEditButton.children.item(1).addEventListener("click", () => {
+            let editItem = this.model.searchList(listId);
+            editItem.editListName(listEditInput.value);
+            editItem = this.model.searchList(listId);
+            listEditButton.remove();
+            this.updateSidebarUI();
+        });
+
+        // List edit cancel
+        listEditButton.children.item(2).addEventListener("click", () => {
+            let editItem = this.model.searchList(listId);
+            listEditButton.remove();
+            this.updateSidebarUI();
+        });
+    }
+
+    handleListDelete(listId) {
+        document.querySelector(`div[data-id="${listId}"]`).remove();
+        this.model.deleteList(listId);
+    }
+
+    initialize() {
+        this.model.lists.forEach((list) => {
+            this.createList(list.id, list.name);
+        });
+        document.querySelector(".list-add-button").addEventListener("click", this.handleListCreate);
+    }
+
+    updateSidebarUI() {
+        const addListButton = (listId, listName) => {
+            this.views.renderListButton(listId, listName);
+
+            document
+                .querySelector(`div[data-id="${listId}"]`)
+                .children.item(2)
+                .children.item(0)
+                .addEventListener("click", () => {this.handleListEdit(listId, listName)});
+
+            document
+                .querySelector(`div[data-id="${listId}"]`)
+                .children.item(2)
+                .children.item(1)
+                .addEventListener("click", () => {this.handleListDelete(listId)});
+        };
+
+        document.querySelector(".navbar").innerHTML = "";
+        this.model.lists.forEach((list) => {
+            addListButton(list.id, list.name);
+        });
+    }
+
+    createList(listId, listName) {
+        if (this.model.searchList(listId) === undefined) {
+            this.model.createList(listId, listName);
+        }
+        this.updateSidebarUI();
     }
 }
