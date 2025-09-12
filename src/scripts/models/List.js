43e51e6a3e95956bcf766b90ef7188ea25c85836
3psilon0 <bestofluck110@gmail.com>
2025-09-12 11:36:11 +0500

    Added reset functions and fixed implementation of initialization of lists and list items

--- src/scripts/models/List.js
+++ src/scripts/models/List.js
@@ -1,14 +1,14 @@
 import storage from '../services/StorageService.js';
 
 class ListItem{
-    constructor(id, parentId, title, desc, dueDate, priority){
+    constructor(id, parentId, title, desc, dueDate, priority, checked = false){
         this.id = id;
         this.parentId = parentId;
         this.title = title;
         this.desc = desc;
         this.dueDate = dueDate;
         this.priority = priority;
-        this.checked = false;
+        this.checked = checked;
     }
 
     toggleCheck() {
@@ -24,36 +24,55 @@ class ListItem{
 }
 
 export default class{
-    constructor(listId, listName, listItems = []){
+    constructor(listId, listName, listItems = [], itemCounter = 1){
         this.id = listId;
         this.name = listName;
-        this.items = listItems;
+        this.itemCounter = itemCounter;
+        
+        this.items = []
+        if(listItems.length != 0) {
+            listItems.forEach((storedItem) => {
+                const retrievedItem = new ListItem(storedItem.id, storedItem.parentId, storedItem.title, storedItem.desc, storedItem.dueDate, storedItem.priority, storedItem.checked);
+                this.items.push(retrievedItem);
+            })
+        }
     }
 
     addItem(itemId, title, desc, dueDate, priority){
+        this.itemCounter += 1;
         const item = new ListItem(itemId, this.id, title, desc, dueDate, priority);
         this.items.push(item);
-        storage.save(`list-${this.id}`, JSON.stringify(this))
+        storage.save(`list-${this.id}`, JSON.stringify(this));
     }
 
     searchItem(itemId){
-        return this.items.find(item => item.id === itemId);
+        return this.items.find(item => item.id === parseInt(itemId));
     }
 
     removeItem(itemId){
         this.items.splice(this.items.indexOf(this.searchItem(itemId)), 1);
-        storage.save(`list-${this.id}`, JSON.stringify(this))
+        storage.save(`list-${this.id}`, JSON.stringify(this));
     }
 
     editItem(itemId, title, desc, dueDate, priority){
         const item = this.items.find(item => item.id === itemId);
         item.edit(title, desc, dueDate, priority);
-        storage.save(`list-${this.id}`, JSON.stringify(this))
+        storage.save(`list-${this.id}`, JSON.stringify(this));
+    }
+
+    toggleItem(itemId){
+        this.searchItem(itemId).toggleCheck();
+        storage.save(`list-${this.id}`, JSON.stringify(this));
     }
 
     editListName(name){
         this.name = name;
-        storage.save(`list-${this.id}`, JSON.stringify(this))
+        storage.save(`list-${this.id}`, JSON.stringify(this));
+    }
+
+    reset(){
+        this.itemCounter = 0;
+        this.items = [];
     }
 
     get nCheckedItems(){
--- src/scripts/models/ListModel.js
+++ src/scripts/models/ListModel.js
@@ -4,18 +4,24 @@ import List from './List.js'
 export default class{
     constructor(){
         this.lists = [];
+        this.listCounter = 0;
         Object.keys(storage.instance).forEach(key => {
-            if(key.slice(0, 4) === 'list'){
+            if(key.slice(0, 5) === 'list-'){
                 const storedList = JSON.parse(storage.instance[key]);
-                const retrievedList = new List(storedList.id, storedList.name, storedList.items);
+                const retrievedList = new List(storedList.id, storedList.name, storedList.items, storedList.itemCounter);
+                console.log(retrievedList);
                 this.lists.push(retrievedList); 
+            }
+            else if(key === 'listCounter'){
+                this.listCounter = parseInt(storage.instance[key]);
             }
         })
         this.lists.sort((a, b) => a.name.localeCompare(b.name));
-        console.log(this.lists);
     }
 
     createList(listId, name) {
+        this.listCounter += 1;
+        storage.save('listCounter', this.listCounter.toString());
         const list = new List(listId, name);
         this.lists.push(list);
         this.lists.sort((a, b) => a.name.localeCompare(b.name));
@@ -23,11 +29,24 @@ export default class{
     }
 
     searchList(listId){
-        return this.lists.find(list => list.id === listId);
+        return this.lists.find(list => list.id === parseInt(listId));
     }
     
     deleteList(listId){
     this.lists.splice(this.lists.indexOf(this.searchList(listId)), 1);
     storage.delete(`list-${listId}`);
     }
+
+    reset(){
+        Object.keys(storage.instance).forEach(key => {
+            if(key.slice(0, 4) === 'list'){
+                this.deleteList(JSON.parse(storage.instance[key]).id); 
+            }
+            else if(key === 'listCounter'){
+                this.listCounter = 0;
+                storage.save('listCounter', this.listCounter.toString());
+            }
+        })
+    }
+
 }
