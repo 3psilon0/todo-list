4cd42f5fdf391103761ececc25f581da8b0dfcf1
3psilon0 <bestofluck110@gmail.com>
2025-09-13 12:54:05 +0500

    1: Added functionality for list item buttons. 2: Content now renders changes or deletions of lists

--- src/scripts/controllers/ListController.js
+++ src/scripts/controllers/ListController.js
@@ -34,6 +34,7 @@
     };
 
     handleListEdit = (e) => {
+        const content = document.querySelector('#content');
         const parentListTag = e.currentTarget.parentElement.parentElement;
         const listId = parentListTag.dataset.id;
         const listName = this.model.searchList(listId).name;
@@ -47,26 +48,32 @@
 
         // List edit confirm
         listEditButton.children.item(1).addEventListener("click", () => {
-            let editItem = this.model.searchList(listId);
+            const editItem = this.model.searchList(listId);
             editItem.editListName(listEditInput.value);
-            editItem = this.model.searchList(listId);
+            if(listId === content.dataset.listId){
+                this.updateContentUI(editItem.id);
+            }
             listEditButton.remove();
             this.updateSidebarUI();
         });
 
         // List edit cancel
         listEditButton.children.item(2).addEventListener("click", () => {
-            let editItem = this.model.searchList(listId);
             listEditButton.remove();
             this.updateSidebarUI();
         });
     }
 
     handleListDelete = (e) => {
+        const content = document.querySelector('#content');
         const listId = e.currentTarget.parentElement.parentElement.dataset.id;
 
-        document.querySelector(`div[data-id="${listId}"]`).remove();
         this.model.deleteList(listId);
+        if(content.dataset.listId === listId){
+            content.innerHTML = '<h1 class="content-empty">No list is selected..</h1>';
+        }
+
+        this.updateSidebarUI();
     }
 
     handleItemCreate = () => {
@@ -93,7 +100,7 @@
         const handleCreateCancel = () => {
             document.querySelector('.form-confirm-button').removeEventListener("click", handleCreateConfirm);
             document.querySelector('.form-cancel-button').removeEventListener("click", handleCreateCancel);
-            
+
             document.querySelector('.item-add-form').reset();
             document.querySelector('.item-add-dialog').close();
         }
@@ -103,6 +110,83 @@
 
         document.querySelector('.form-confirm-button').addEventListener("click", handleCreateConfirm);
         document.querySelector('.form-cancel-button').addEventListener("click", handleCreateCancel);
+    }
+
+    handleItemEdit = (e) => {
+        const itemId = e.target.parentElement.parentElement.parentElement.parentElement.dataset.itemId;
+        const parentId = e.target.parentElement.parentElement.parentElement.parentElement.dataset.parentId;
+        const currentList = this.model.searchList(parentId);
+        const form = document.querySelector('.item-add-form');
+        
+        const handleEditConfirm = (e) => {
+            e.preventDefault();
+            
+            if(form.checkValidity()){
+                document.querySelector('.form-confirm-button').removeEventListener("click", handleEditConfirm);
+                document.querySelector('.form-cancel-button').removeEventListener("click", handleEditCancel);
+
+                const desc = document.querySelector('#item-desc-input').value === '' ? 'No description' : document.querySelector('#item-desc-input').value;
+                const dueDate = new Date(document.querySelector('#item-dueDate-input').value).toLocaleString('Us-en', {dateStyle: 'medium'});
+
+                currentList.editItem(itemId, document.querySelector('#item-title-input').value, desc, dueDate, document.querySelector(`input[name='prio']:checked`).value);
+                document.querySelector('.item-add-dialog').close();
+                this.updateContentUI(parentId);
+                form.reset();
+            }
+        }
+
+        const handleEditCancel = () => {
+            document.querySelector('.form-confirm-button').removeEventListener("click", handleEditConfirm);
+            document.querySelector('.form-cancel-button').removeEventListener("click", handleEditCancel);
+
+            document.querySelector('.item-add-form').reset();
+            document.querySelector('.item-add-dialog').close();
+        }
+        
+        const initalizeForm = () => {
+            const listItem = currentList.searchItem(itemId);
+            document.querySelector('#item-title-input').value = listItem.title;
+            document.querySelector('#item-desc-input').value = listItem.desc;
+            const dueDate = new Date(listItem.dueDate).toLocaleString('en-CA', {year: 'numeric', month: '2-digit', day: '2-digit'});
+            document.querySelector('#item-dueDate-input').value = dueDate;
+            document.querySelector(`#prio-${listItem.priority}-input`).checked = true;
+        }
+
+        document.querySelector('.dialog-title').innerHTML = "Edit task";
+        initalizeForm();
+        document.querySelector('.item-add-dialog').showModal();
+
+        document.querySelector('.form-confirm-button').addEventListener("click", handleEditConfirm);
+        document.querySelector('.form-cancel-button').addEventListener("click", handleEditCancel);        
+    }
+
+    handleItemDelete = (e) => {
+        const itemId = e.target.parentElement.parentElement.parentElement.parentElement.dataset.itemId;
+        const parentId = e.target.parentElement.parentElement.parentElement.parentElement.dataset.parentId;
+        const currentList = this.model.searchList(parentId);
+
+        currentList.removeItem(itemId);
+        this.updateContentUI(parentId);
+    }
+
+    handleItemDropdown = (e) => {
+        const itemId = e.target.parentElement.parentElement.parentElement.parentElement.dataset.itemId;
+
+        const itemElement = document.querySelector(`div[data-item-id="${itemId}"]`);
+        e.target.classList.toggle('dropped');
+        const descElement = itemElement.children.item(1);
+        
+        if(descElement.style.maxHeight){
+            descElement.style.maxHeight = null;
+            descElement.style.padding = null;
+            descElement.style.opacity = null;
+        }
+        else {
+            descElement.style.maxHeight = descElement.scrollHeight + "px";
+            descElement.style.padding = "10px";
+            descElement.style.opacity = "1";
+        }
+        
     }
 
     handleItemToggle = (e) => {
@@ -144,6 +228,9 @@
             this.views.renderListItem(item.id, item.parentId, item.title, item.desc, item.dueDate, item.priority, item.checked);
 
             document.querySelector(`div[data-item-id="${item.id}"]`).children.item(0).children.item(0).children.item(1).addEventListener("click", this.handleItemToggle);
+            document.querySelector(`div[data-item-id="${item.id}"]`).children.item(0).children.item(2).children.item(0).addEventListener("click", this.handleItemDropdown);
+            document.querySelector(`div[data-item-id="${item.id}"]`).children.item(0).children.item(2).children.item(1).addEventListener("click", this.handleItemEdit);
+            document.querySelector(`div[data-item-id="${item.id}"]`).children.item(0).children.item(2).children.item(2).addEventListener("click", this.handleItemDelete);
         });
     }
 
